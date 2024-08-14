3eaa738e13bd7eee8b09dc2a5ac193fc8c04a8ab
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-08-14 20:33:30 +0300

    Ще один варіант як не треба робити. Через проміси з блокуванням циклу виконання та перевищенням пам'яті

---
+++ 02-blocking-event-loop/fibonachi-out-of-memory.js
@@ -0,0 +1,15 @@
+setTimeout(() => console.log("timeout"), 0);
+
+function fib(n) {
+  return new Promise((resolve, reject) => {
+    if (n === 0 || n === 1) {
+      return resolve(n);
+    }
+    setImmediate(() => {
+      Promise.all([fib(n - 1), fib(n - 2)]).then(([fib1, fib2]) =>
+        resolve(fib1 + fib2)
+      );
+    });
+  });
+}
+fib(40).then((result) => console.log(result));

93cb81f17b44ae6e4147fde9a0614bbee601aed8
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-08-19 02:52:05 +0300

    Розбираю основні методи в events

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -2,13 +2,24 @@ import { EventEmitter } from "events";
 
 const emitter = new EventEmitter();
 
-emitter.on("timeout", () => {
-  console.log("Timeout event");
+emitter.on("timeout", (secondsQty) => {
+  console.log(`Timeout event in ${secondsQty} seconds`);
 });
 
 setTimeout(() => {
-  emitter.emit("timeout");
+  emitter.emit("timeout", 1);
 }, 1000);
 setTimeout(() => {
-  emitter.emit("timeout");
+  emitter.emit("timeout", 2);
 }, 2000);
+
+emitter.once("singleEvents", () => {
+  console.log(`single event occurred`);
+});
+
+setTimeout(() => {
+  emitter.emit("singleEvents", 1);
+}, 500);
+setTimeout(() => {
+  emitter.emit("singleEvents", 2);
+}, 1500);
